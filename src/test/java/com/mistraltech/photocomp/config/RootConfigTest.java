ba755716384eeef477c57d54356771ddb191b3e5
Richard Wilson <richardjwilson@users.noreply.github.com>
2015-11-08 16:09:17 +0000

    Remove explicit loader class from config tests.
    The loader is correctly inferred from the other annotation parameters and
    need not be stated explicitly.

--- src/test/java/com/mistraltech/photocomp/config/RootConfigTest.java
+++ src/test/java/com/mistraltech/photocomp/config/RootConfigTest.java
@@ -1,25 +1,19 @@
 package com.mistraltech.photocomp.config;
 
 import com.mistraltech.photocomp.repository.CompetitionConfigRepository;
-import com.mistraltech.photocomp.repository.DummyCompetitionConfigRepository;
-import com.mistraltech.photocomp.repository.StubCompetitionConfigRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsInstanceOf.instanceOf;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = RootConfig.class, loader = AnnotationConfigContextLoader.class)
+@ContextConfiguration(classes = RootConfig.class)
 public class RootConfigTest {
 
     @Autowired
--- src/test/java/com/mistraltech/photocomp/config/StubRootConfigTest.java
+++ src/test/java/com/mistraltech/photocomp/config/StubRootConfigTest.java
@@ -8,14 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = RootConfig.class, loader = AnnotationConfigContextLoader.class)
+@ContextConfiguration(classes = RootConfig.class)
 @ActiveProfiles("stub")
 public class StubRootConfigTest {
 
